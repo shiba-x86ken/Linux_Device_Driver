1b603af24798a5f0488286ea79671688a42857fb
shiba-x86ken <38150179+shiba-x86ken@users.noreply.github.com>
2018-04-27 09:45:49 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<linux/module.h>
+
+MODULE_AUTHOR("shiba_x86ken");
+MODULE_LICENSE("GPL");
+
+int init_module(void){
+    printk(KERN_INFO "Hello\n");
+    return 0;
+}
+
+void cleanup_module(void){
+    printk(KERN_INFO "Bye\n");
+}
