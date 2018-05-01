5dce9aa808d77859bd5e3986ac4a7c3a046a8011
shiba-x86ken <38150179+shiba-x86ken@users.noreply.github.com>
2018-05-01 10:16:07 +0900

    Add files via upload

---
+++ do_not_stop.c
@@ -0,0 +1,15 @@
+#include<linux/module.h>
+
+MODULE_AUTHOR("shiba_x86ken");
+MODULE_LICENSE("GPL");
+
+int init_module(void) {
+    while(1) {
+        printk(KERN_INFO "Hello\n");
+    }
+    return 0;
+}
+
+void cleanup_module(void) {
+    printk(KERN_INFO "Bye\n");
+}
